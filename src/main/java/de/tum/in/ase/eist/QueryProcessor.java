4771026c20a9c561577b830f2a9ebfce5af3e6ab
R020080\yazan <yazan.aljlelati@hotmail.com>
2022-07-13 15:03:10 +0200

    13th try

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -15,7 +15,7 @@ public class QueryProcessor {
            return "MyTeam";
         } else { // TODO extend the programm here
             // Add a new comment here, please don't remove
-            // 12th try
+            // 13th try
             return "";
         }
     }
